8751e53d246cb545a46d23676a4f72333b5e81a0
Joseph Huynh <joe.huynh@gmail.com>
2012-02-14 02:46:54 -0800

    Basic framework is set up. Image search algorithm needs work.

--- src/clickbot/ImageFind.java
+++ src/clickbot/ImageFind.java
@@ -1,12 +1,146 @@
 package clickbot;
 
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 /**
- * Class for finding a distinct image on the screen
+ * Class for finding a distinct image on the screen. Works by getting
+ * coordinates for a top-left and a bottom-right anchor, then searching all the
+ * pixels in between for the image.
  * 
- * @author Joseph
+ * @author Joseph Huynh
  */
 public class ImageFind {
-	
+	private Point topLeft; // Coordinates of top-left anchor
+	private Point botRight; // Coordinates of bottom-right anchor
+	private BufferedImage tl;
+	private BufferedImage br;
+	private BufferedImage screen;
+	private Robot bot;
+	private Dimension screenSize;
+	private Rectangle screenRectangle;
+
+	/**
+	 * Constructor when both anchor images are provided
+	 * 
+	 * @param tl
+	 *            Image of top-left anchor
+	 * @param br
+	 *            Image of bottom-right anchor
+	 * @throws Exception
+	 */
+	ImageFind(BufferedImage tl, BufferedImage br) throws Exception {
+		bot = new Robot();
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenRectangle = new Rectangle(screenSize);
+		screen = bot.createScreenCapture(screenRectangle);
+
+		// Save as PNG
+		ImageIO.write(screen, "png", new File("screencapture.png"));
+
+		topLeft = findImg(tl, screen);
+		botRight = findImg(br, screen);
+
+		if (topLeft == null || botRight == null) {
+			throw new Exception("Can't find anchors");
+		} else {
+			System.out.println("TL anchor: (" + topLeft.getX() + ","
+					+ topLeft.getY() + ")");
+			System.out.println("BR anchor: (" + botRight.getX() + ","
+					+ botRight.getY() + ")");
+		}
+	}
+
+	/**
+	 * Constructor when only the background image is provided
+	 * 
+	 * @param background
+	 */
+	ImageFind(BufferedImage background) {
+
+	}
+
+	/**
+	 * Searches for the given image
+	 * 
+	 * @param img
+	 *            Image to search for
+	 * @return Coordinates of the image if it is found, null if not found
+	 */
+	private Point findImg(BufferedImage img, BufferedImage bigImage) {
+		// look for all (x,y) positions where target appears in desktop
+		int x0, y0, bigW, bigH, imgW, imgH;
+		int maxRow = 0;
+		int maxCol = 0;
+
+		bigW = bigImage.getWidth();
+		bigH = bigImage.getHeight();
+		imgW = img.getWidth();
+		imgH = img.getHeight();
+
+		for (int y = 0; y < bigH - imgH; y++) {
+			xSearch: for (int x = 0; x < bigW - imgW; x++) {
+				if (Math.abs(bigImage.getRGB(x, y) - img.getRGB(0, 0)) < 10) {
+					// System.out.println("Found possible match at (" + x + ","
+					// + y + ")");
+					x0 = x;
+					y0 = y;
+					for (int j = 0; j < imgH; j++) {
+						for (int i = 0; i < imgW; i++) {
+							if (Math.abs(img.getRGB(i, j)
+									- bigImage.getRGB(x + i, y + j)) > 10) {
+								continue xSearch;
+							}
+							if (j > maxRow) {
+								maxRow = j;
+								if (i > maxCol)
+									maxCol = i;
+							}
+						}
+					}
+					return new Point(x0, y0);
+				}
+			}
+		}
+		System.out.println("Max number of matches is " + maxRow + " rows and "
+				+ maxCol + " columns");
+		return null;
+	}
+
+	/**
+	 * Searches the screen for the given anchors. Used when the user may have
+	 * moved the game window.
+	 */
+	void findAnchors() {
+		screen = bot.createScreenCapture(screenRectangle);
+		topLeft = findImg(tl, screen);
+		botRight = findImg(br, screen);
+	}
+
+	// computes the root mean squared error between a rectangular window in
+	// bigImg and target.
+	double imageDistance(BufferedImage bigImg, int bx, int by,
+			BufferedImage target) {
+		double dist = 0.0;
+		for (int y = 0; y < target.getHeight(); y++) {
+			for (int x = 0; x < target.getWidth(); x++) {
+				// assume RGB images...
+				for (int colorChannel = 0; colorChannel < 3; colorChannel++) {
+					dist += Math.pow(target.getRGB(x, y) - bigImg.getRGB(x, y),
+							2);
+				}
+			}
+		}
+		return Math.sqrt(dist) / target.getWidth() / target.getHeight();
+	}
 	
 	
 }
--- src/clickbot/MainWindow.java
+++ src/clickbot/MainWindow.java
@@ -1,7 +1,10 @@
 package clickbot;
 
 import java.awt.EventQueue;
+import java.awt.Robot;
+
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,14 +18,21 @@ import javax.swing.SwingConstants;
 
 /**
  * Main screen
+ * 
  * @author Joseph Huynh
- *
+ * 
  */
 public class MainWindow {
 
 	private JFrame frmClickbotByJoseph;
 	private BufferedImage im;
+	private BufferedImage tl;
+	private BufferedImage br;
+	private ImageFind imgF;
 	private JLabel statLbl;
+	private JButton btnStart;
+	private Robot bot;
+	private boolean running = false;
 
 	/**
 	 * Launch the application.
@@ -42,47 +52,91 @@ public class MainWindow {
 
 	/**
 	 * Create the application.
+	 * @throws Exception 
 	 */
-	public MainWindow() {
+	public MainWindow() throws Exception {
 		initialize();
+		load();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
 		frmClickbotByJoseph = new JFrame();
 		frmClickbotByJoseph.setTitle("ClickBot by Joseph Huynh");
 		frmClickbotByJoseph.setBounds(100, 100, 290, 110);
 		frmClickbotByJoseph.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmClickbotByJoseph.getContentPane().setLayout(null);
-		
-		statLbl = new JLabel("Status: No image loaded");
+
+		statLbl = new JLabel("Status: Ready");
 		statLbl.setHorizontalAlignment(SwingConstants.CENTER);
 		statLbl.setBounds(10, 11, 254, 14);
 		frmClickbotByJoseph.getContentPane().add(statLbl);
-		
-		JButton btnStart = new JButton("Start");
+
+		btnStart = new JButton("Start");
 		btnStart.addMouseListener(new StartListener());
 		btnStart.setBounds(93, 38, 89, 23);
 		frmClickbotByJoseph.getContentPane().add(btnStart);
 	}
-	
+
+	/**
+	 * Loads image files
+	 * @throws Exception 
+	 */
+	private void load() throws Exception {
+		try {
+			im = ImageIO.read(new File("src/clickfast.png"));
+			tl = ImageIO.read(new File("src/tl.png"));
+			br = ImageIO.read(new File("src/br.png"));
+			System.out.println("Files loaded successfully");
+			System.out.println("tl: "+tl.getWidth()+"x"+tl.getHeight());
+			System.out.println("br: "+br.getWidth()+"x"+br.getHeight());
+			imgF = new ImageFind(tl, br);
+		} catch (IOException e) {
+			e.printStackTrace();
+			status("Unable to load files");
+		}
+	}
+
 	/**
 	 * Starts bot
 	 */
-	private void start(){
-		
+	private void start() {
+		running = true;
+		btnStart.setText("Stop");
 	}
-	
+
+	/**
+	 * Stops bot
+	 */
+	private void stop() {
+		running = false;
+		btnStart.setText("Start");
+	}
+
 	/**
 	 * Event handler for 'Start' button
 	 */
 	private class StartListener extends MouseAdapter {
-		
+
 		public void mouseClicked(MouseEvent e) {
-			start();
+			if (!running)
+				start();
+			else
+				stop();
 		}
+	}
+
+	/**
+	 * Method for updating the status text
+	 * 
+	 * @param str
+	 *            Text to display
+	 */
+	private void status(String str) {
+		statLbl.setText("Status: " + str);
 	}
 
 }
